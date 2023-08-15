2ccf04c16d30086931e02220fd24ed38f341c181
Leo Chen <yinchiuan@google.com>
2023-08-15 06:26:06 +0000

    libhwc2.1: libdisplayinterface: override sendHistogramChannelIoctl
    If control is REQUEST, call histogram_channel_request_ioctl which
    creates the drm event to request the histogram data.
    If control is CANCEL, call histogram_channel_cancel_ioctl which
    frees the drm event to cancel the histogram data request.
    
    Bug: 255986866
    Bug: 227384721
    Test: use aidl_hist_client to test multi channel histogram
    Change-Id: I2d87aa7e92128ac0430ae12ad3a913fc4db960b4
    Signed-off-by: Leo Chen <yinchiuan@google.com>

--- libhwc2.1/libdisplayinterface/ExynosDisplayDrmInterfaceModule.h
+++ libhwc2.1/libdisplayinterface/ExynosDisplayDrmInterfaceModule.h
@@ -23,10 +23,16 @@
 
 namespace zuma {
 
-    class ExynosPrimaryDisplayDrmInterfaceModule : public gs201::ExynosPrimaryDisplayDrmInterfaceModule {
+class ExynosPrimaryDisplayDrmInterfaceModule
+      : public gs201::ExynosPrimaryDisplayDrmInterfaceModule {
 public:
-        ExynosPrimaryDisplayDrmInterfaceModule(ExynosDisplay *exynosDisplay);
-    };
+    ExynosPrimaryDisplayDrmInterfaceModule(ExynosDisplay *exynosDisplay);
+// TODO: b/295990513 - Remove the if defined after kernel prebuilts are merged.
+#if defined(EXYNOS_HISTOGRAM_CHANNEL_REQUEST)
+    virtual int32_t sendHistogramChannelIoctl(HistogramChannelIoctl_t control,
+                                              uint8_t channelId) const override;
+#endif
+};
 
 using ExynosExternalDisplayDrmInterfaceModule = gs201::ExynosExternalDisplayDrmInterfaceModule;
 
